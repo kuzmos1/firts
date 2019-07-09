4332d3e19e614e4c0219b2561dea2193d3e43399
Küzmös Sándor <sandor.kuzmos54@gmail.com>
2019-07-09 11:32:49 +0200

    Hausaufgabe : Bildung application

---
+++ target/generated-sources/swagger/src/main/java/io/swagger/model/School.java
@@ -0,0 +1,132 @@
+package io.swagger.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * School
+ */
+@Validated
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2019-07-09T00:18:20.883+02:00[Europe/Berlin]")
+
+public class School   {
+  @JsonProperty("schoolname")
+  private String schoolname = null;
+
+  @JsonProperty("preis")
+  private Integer preis = null;
+
+  @JsonProperty("address")
+  private String address = null;
+
+  public School schoolname(String schoolname) {
+    this.schoolname = schoolname;
+    return this;
+  }
+
+  /**
+   * Get schoolname
+   * @return schoolname
+  **/
+  @ApiModelProperty(required = true, value = "")
+  @NotNull
+
+
+  public String getSchoolname() {
+    return schoolname;
+  }
+
+  public void setSchoolname(String schoolname) {
+    this.schoolname = schoolname;
+  }
+
+  public School preis(Integer preis) {
+    this.preis = preis;
+    return this;
+  }
+
+  /**
+   * Get preis
+   * @return preis
+  **/
+  @ApiModelProperty(value = "")
+
+
+  public Integer getPreis() {
+    return preis;
+  }
+
+  public void setPreis(Integer preis) {
+    this.preis = preis;
+  }
+
+  public School address(String address) {
+    this.address = address;
+    return this;
+  }
+
+  /**
+   * Get address
+   * @return address
+  **/
+  @ApiModelProperty(value = "")
+
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    School school = (School) o;
+    return Objects.equals(this.schoolname, school.schoolname) &&
+        Objects.equals(this.preis, school.preis) &&
+        Objects.equals(this.address, school.address);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(schoolname, preis, address);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class School {\n");
+    
+    sb.append("    schoolname: ").append(toIndentedString(schoolname)).append("\n");
+    sb.append("    preis: ").append(toIndentedString(preis)).append("\n");
+    sb.append("    address: ").append(toIndentedString(address)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
---
+++ target/generated-sources/swagger/src/main/java/io/swagger/model/Student.java
@@ -0,0 +1,186 @@
+package io.swagger.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * Student
+ */
+@Validated
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2019-07-09T00:18:20.883+02:00[Europe/Berlin]")
+
+public class Student   {
+  @JsonProperty("id")
+  private Long id = null;
+
+  @JsonProperty("firstname")
+  private String firstname = null;
+
+  @JsonProperty("lastname")
+  private String lastname = null;
+
+  @JsonProperty("age")
+  private Integer age = null;
+
+  @JsonProperty("address")
+  private String address = null;
+
+  public Student id(Long id) {
+    this.id = id;
+    return this;
+  }
+
+  /**
+   * Get id
+   * @return id
+  **/
+  @ApiModelProperty(required = true, value = "")
+  @NotNull
+
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Student firstname(String firstname) {
+    this.firstname = firstname;
+    return this;
+  }
+
+  /**
+   * Get firstname
+   * @return firstname
+  **/
+  @ApiModelProperty(required = true, value = "")
+  @NotNull
+
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public void setFirstname(String firstname) {
+    this.firstname = firstname;
+  }
+
+  public Student lastname(String lastname) {
+    this.lastname = lastname;
+    return this;
+  }
+
+  /**
+   * Get lastname
+   * @return lastname
+  **/
+  @ApiModelProperty(required = true, value = "")
+  @NotNull
+
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public void setLastname(String lastname) {
+    this.lastname = lastname;
+  }
+
+  public Student age(Integer age) {
+    this.age = age;
+    return this;
+  }
+
+  /**
+   * Get age
+   * @return age
+  **/
+  @ApiModelProperty(required = true, value = "")
+  @NotNull
+
+
+  public Integer getAge() {
+    return age;
+  }
+
+  public void setAge(Integer age) {
+    this.age = age;
+  }
+
+  public Student address(String address) {
+    this.address = address;
+    return this;
+  }
+
+  /**
+   * Get address
+   * @return address
+  **/
+  @ApiModelProperty(required = true, value = "")
+  @NotNull
+
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Student student = (Student) o;
+    return Objects.equals(this.id, student.id) &&
+        Objects.equals(this.firstname, student.firstname) &&
+        Objects.equals(this.lastname, student.lastname) &&
+        Objects.equals(this.age, student.age) &&
+        Objects.equals(this.address, student.address);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, firstname, lastname, age, address);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class Student {\n");
+    
+    sb.append("    id: ").append(toIndentedString(id)).append("\n");
+    sb.append("    firstname: ").append(toIndentedString(firstname)).append("\n");
+    sb.append("    lastname: ").append(toIndentedString(lastname)).append("\n");
+    sb.append("    age: ").append(toIndentedString(age)).append("\n");
+    sb.append("    address: ").append(toIndentedString(address)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
